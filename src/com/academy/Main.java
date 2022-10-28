1535e329fd823ede4312d43f3b69a63aaadc5c69
SvitlanaAleks <svitlanaalex8@gmail.com>
2022-10-28 18:59:17 +0300

    new branch and 2 objects

--- src/com/academy/Main.java
+++ src/com/academy/Main.java
@@ -10,6 +10,9 @@ public class Main {
         Lectures lectures1 = new Lectures();
         Lectures lectures2 = new Lectures();
 
+        Lectures lectures3 = new Lectures();
+        Lectures lectures4 = new Lectures();
+
 
         System.out.println(Lectures.counter);
         System.out.println(Lectures.counter);
