cb649bb5f2b184ef1012348804d43ca2002343ad
SvitlanaAleks <svitlanaalex8@gmail.com>
2022-11-03 15:29:15 +0200

    2 new objects

--- src/com/academy/Main.java
+++ src/com/academy/Main.java
@@ -1,6 +1,7 @@
 package com.academy;
 
 
+import com.academy.models.Courses;
 import com.academy.models.Lectures;
 
 public class Main {
@@ -13,9 +14,14 @@ public class Main {
         Lectures lectures3 = new Lectures();
         Lectures lectures4 = new Lectures();
 
+        // 6 лекція
+        Courses courses1=new Courses();
+
+        Lectures lectures5= new Lectures();
 
 
-        System.out.println(Lectures.counter);
+
+        System.out.println(Courses.counter);
         System.out.println(Lectures.counter);
 
 
--- src/com/academy/models/Lectures.java
+++ src/com/academy/models/Lectures.java
@@ -3,9 +3,11 @@ package com.academy.models;
 public class Lectures {
     private static long ID;
     public static long counter;
+    public static String idCourses;
    public Lectures() {
         ID = counter++;
-    }
+
+   }}
 
 
-}
+
